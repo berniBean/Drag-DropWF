e2c23ff9b9bc9818f02b408799efd565dc1f15a4
berniBean <ernesto_fope55@hotmail.com>
2021-10-25 18:31:21 -0500

    carpeta Singleton

---
+++ Singleton/ChocoPdfs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class ChocoPdfs
+    {
+        public ChocoPdfs(string name, string fullName)
+        {
+            _name = name;
+            _fullName = fullName;
+            
+        }
+        public ChocoPdfs()
+        {
+
+        }
+        public string _name { get; set; }
+        public string _fullName { get; set; }
+        
+    }
+}
